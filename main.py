3a677ea6387c454eba1685fa074089d6a674e001
Ben Bauer <ben.bauer1@gmail.com>
2021-08-06 16:58:26 -0500

    retrieves blog data

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, url_for
 from flask.templating import render_template
+import requests
 
 app = Flask(__name__)
 
@@ -17,6 +18,9 @@ def contact():
 
 @app.route('/post')
 def post():
+    blogdata = requests.get("https://api.npoint.io/43644ec4f0013682fc0d")
+    blog = blogdata.json()
+    print(blog)
     return(render_template('post.html'))
 
 if __name__ == "__main__":
